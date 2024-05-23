a694b1103706d7e21a4583e9f118802e07d41a57
Aleksandr <aleksandr-s86>
2024-05-23 04:36:07 +0300

    hw10

---
+++ data.js
@@ -0,0 +1,22 @@
+const dogs = `[
+    {
+        "message": "https://images.dog.ceo/breeds/puggle/IMG_192117.jpg",
+        "status": "success"
+    },
+    {
+        "message": "https://images.dog.ceo/breeds/mountain-bernese/n02107683_342.jpg",
+        "status": "success"
+    },
+    {
+        "message": "https://images.dog.ceo/breeds/bullterrier-staffordshire/n02093256_2557.jpg",
+        "status": "success"
+    },
+    {
+        "message": "https://images.dog.ceo/breeds/boxer/n02108089_1511.jpg",
+        "status": "success"
+    },
+    {
+        "message": "https://images.dog.ceo/breeds/terrier-norfolk/n02094114_161.jpg",
+        "status": "success"
+    }
+]` 
